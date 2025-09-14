3815a1b6b72b783cd2c2898b0ee570ddcab0adbd
andrew.forkes <andrew.forkes@ambroseconstruct.com.au>
2025-09-14 19:13:30 +1000

    langchain api version added

--- api/index-langchain.js
+++ api/index-langchain.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 8000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../static')));
+app.use(express.static(path.join(__dirname, '../static'), { index: false }));
 
 // Initialize OpenAI
 const llm = new ChatOpenAI({
